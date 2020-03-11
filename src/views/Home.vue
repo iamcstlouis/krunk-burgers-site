4e6d858be04b073cd2226741d9b0beed56b14c35
Chantal St Louis <chantalstlouis@akauk.com>
2020-03-11 11:38:07 +0000

    Adds site TopNav component without styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <HomepageHero />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 import HomepageHero from "@/components/HomePageHero.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
     HomepageHero
   }
 };
