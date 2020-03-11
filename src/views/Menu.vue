e0e296636dceac8ee58a41eab67f7ac6add1e41b
Chantal St Louis <chantalstlouis@akauk.com>
2020-03-11 17:30:47 +0000

    Adds Menu page / View and component

---
+++ src/views/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="menu">
+    <div class="container">
+      <!-- Deliveroo Widget -->
+      <DeliverooWidget />
+    </div>
+  </div>
+</template>
+
+<script>
+import DeliverooWidget from "@/components/DeliverooWidget.vue";
+
+export default {
+  name: "Menu",
+  components: {
+    DeliverooWidget
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.menu {
+    background-color: #F9B450;
+}
+</style>
