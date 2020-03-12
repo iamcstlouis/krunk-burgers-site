184428fdcd2c8071bd2a7801c6f9ec2a9425dd76
Chantal St Louis <chantalstlouis@akauk.com>
2020-03-12 13:07:18 +0000

    Adds formatting and styling to BurgerCard component

--- src/components/BurgerCard.vue
+++ src/components/BurgerCard.vue
@@ -8,34 +8,57 @@
 
 <script>
 export default {
-    name: "BurgerCard"
-}
+  name: "BurgerCard"
+};
 </script>
 
 <style lang="scss" scoped>
-    .burger-card {
-        max-width: 28rem;
+.burger-card {
+  margin-top: 5rem;
 
-        img {
-            width: 100%;
-            height: auto;
-        }
+  @media (min-width: 768px) {
+    max-width: 28rem;
+  }
 
-        .meal-info {
-            text-align: left;
-            margin-top: 2.4rem;
-
-        h2{
-            font-family: "dnk";
-            font-size: 24px;
-            color: #731550;
-        }
-
-        p {
-            font-size: 16px;
-            color: #731550;
-            margin-top: .5rem;
-        }
+  @media (min-width: 992px) {
+    &:last-child {
+      margin-right: auto;
+      margin-left: 1rem;
     }
+  }
+
+  @media (min-width: 1024px) {
+    &:last-child {
+      margin-left: 0;
     }
+  }
+
+  @media (min-width: 1200px) {
+    &:last-child {
+      margin-left: 5rem;
+    }
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  .meal-info {
+    text-align: left;
+    margin-top: 2.4rem;
+
+    h2 {
+      font-family: "dnk";
+      font-size: 24px;
+      color: #731550;
+    }
+
+    p {
+      font-size: 16px;
+      color: #731550;
+      margin-top: 0.5rem;
+    }
+  }
+}
 </style>
--- src/components/MealDeals.vue
+++ src/components/MealDeals.vue
@@ -1,30 +1,121 @@
 <template>
   <section class="meal-deals">
     <div class="container">
-      
       <div class="hdr-section">
         <h1>Meal Deals</h1>
         <h3>Meals served with regular fries and a soft drink</h3>
       </div>
 
       <div class="cards">
+        <!-- Card 01 -->
         <BurgerCard>
-          <img src="./../assets/menu/burgers/beef-burger-1.jpg" alt="" class="card-img">
-          <div class="meal-info">
+          <a
+            href="https://deliveroo.co.uk/menu/london/croydon/krunk?day=today&postcode=SE256NX&time=ASAP"
+            target="_blank"
+          >
+            <img
+              src="./../assets/menu/burgers/beef-burger-1.jpg"
+              alt=""
+              class="card-img"
+            />
+            <div class="meal-info">
               <h2>Krunk Burger</h2>
               <p class="meal-desc">
-                2 x smashed beef patties, 2 x slices of cheese, topped with Krunk burger sauce, pickles, lettuce, tomato & onion
+                2 x smashed beef patties, 2 x slices of cheese, topped with
+                Krunk burger sauce, pickles, lettuce, tomato & onion
               </p>
-          </div>
+            </div>
+          </a>
+        </BurgerCard>
+
+        <!-- Card 02 -->
+        <BurgerCard>
+          <a
+            href="https://deliveroo.co.uk/menu/london/croydon/krunk?day=today&postcode=SE256NX&time=ASAP"
+            target="_blank"
+          >
+            <img
+              src="./../assets/menu/burgers/beef-burger-2.jpg"
+              alt=""
+              class="card-img"
+            />
+            <div class="meal-info">
+              <h2>Plain Jane</h2>
+              <p class="meal-desc">
+                2 x smashed beef patties, topped with Krunk burger sauce,
+                pickles, lettuce, tomato & onion
+              </p>
+            </div>
+          </a>
+        </BurgerCard>
+
+        <!-- Card 03 -->
+        <BurgerCard>
+          <a
+            href="https://deliveroo.co.uk/menu/london/croydon/krunk?day=today&postcode=SE256NX&time=ASAP"
+            target="_blank"
+          >
+            <img
+              src="./../assets/menu/burgers/beef-burger-3.jpg"
+              alt=""
+              class="card-img"
+            />
+            <div class="meal-info">
+              <h2>Juicy Lucy</h2>
+              <p class="meal-desc">
+                Melt in the middle cheese, relish & Krunk burger sauce
+              </p>
+            </div>
+          </a>
+        </BurgerCard>
+
+        <!-- Card 04 -->
+        <BurgerCard>
+          <a
+            href="https://deliveroo.co.uk/menu/london/croydon/krunk?day=today&postcode=SE256NX&time=ASAP"
+            target="_blank"
+          >
+            <img
+              src="./../assets/menu/burgers/beef-burger-4.jpg"
+              alt=""
+              class="card-img"
+            />
+            <div class="meal-info">
+              <h2>Spicy Lucy</h2>
+              <p class="meal-desc">
+                Melt in the middle cheese, relish, jalepenos, chipotle sauce
+              </p>
+            </div>
+          </a>
+        </BurgerCard>
+
+        <!-- Card 05 -->
+        <BurgerCard>
+          <a
+            href="https://deliveroo.co.uk/menu/london/croydon/krunk?day=today&postcode=SE256NX&time=ASAP"
+            target="_blank"
+          >
+            <img
+              src="./../assets/menu/burgers/beef-burger-5.jpg"
+              alt=""
+              class="card-img"
+            />
+            <div class="meal-info">
+              <h2>BBQ Bang</h2>
+              <p class="meal-desc">
+                2 x smashed beef patties, 2 x slices of cheese, topped with bbq
+                sauce, pickles, lettuce, tomato & onion
+              </p>
+            </div>
+          </a>
         </BurgerCard>
       </div>
-      
     </div>
   </section>
 </template>
 
 <script>
-import BurgerCard from '@/components/BurgerCard.vue';
+import BurgerCard from "@/components/BurgerCard.vue";
 
 export default {
   name: "MealDeals",
@@ -68,7 +159,24 @@ export default {
     }
 
     .cards {
-      margin-top: 9.3rem;
+      // margin-top: 5rem;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+
+      @media (min-width: 992px) {
+        margin-top: 1rem;
+      }
+
+      @media (min-width: 1024px) and (max-width: 1200px) {
+        width: 85%;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+      @media (min-width: 1200px) {
+        width: 100%;
+      }
     }
   }
 }
