2241f9d9b85330ea315ff49520ba14b7ac5f7dc3
Chantal St Louis <chantalstlouis@akauk.com>
2020-03-12 15:16:07 +0000

    Adds MealsSeparator component

---
+++ src/components/MealsSeparator.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="meals-separator">
+        <div class="content-wrapper">
+            <div class="sparator"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MealsSeparator"
+}
+</script>
+
+<style lang="scss" scoped>
+.meals-separator {
+    .content-wrapper {
+        padding: 9.6rem 0;
+        
+        .meals-separator {
+            width: 100%;
+            height: .2rem;
+        }
+    }
+}
+</style>
