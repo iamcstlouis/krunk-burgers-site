4f55654022fe31559db9b775eb5bfad120e84985
Chantal St Louis <chantalstlouis@akauk.com>
2020-04-10 15:00:13 +0100

    Removes commented code from Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <HomepageHero />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
 import HomepageHero from "@/components/HomePageHero.vue";
 
 export default {
   name: "Home",
   components: {
-    // HelloWorld,
     HomepageHero
   }
 };
