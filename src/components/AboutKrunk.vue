fba26eaec35c2e03651e5d1293c67d392a6a5947
Chantal St Louis <chantalstlouis@akauk.com>
2020-04-12 19:12:44 +0100

    Adds AboutKrunk component to About view

---
+++ src/components/AboutKrunk.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="about-krunk">
+    <b-container>
+      <div class="content-wrapper">
+        <div class="about-hdr">
+          <h1>About</h1>
+        </div>
+
+        <div class="about-main">
+          <div class="content-wrapper">
+            <h2>In the Krunk kitchen...</h2>
+            <p>Duis metus ex, malesuada at tempus ut, laoreet sit amet orci. Phasellus lobortis nibh et sapien facilisis malesuada. Phasellus euismod eros at elit euismod, eget blandit tortor pretium. Vivamus sit amet mauris eros. Nam eget lacus vitae purus iaculis dictum sit amet eget velit. Sed vel rutrum leo. Sed vel fermentum lacus, at bibendum metus. Cras tristique ac urna ut egestas. Nam mattis sit amet diam vel tempor. Pellentesque at elit pharetra, tempor nulla eget, blandit sem. Morbi sollicitudin mauris massa, ac tincidunt ipsum bibendum nec. Etiam in ipsum non tortor interdum bibendum. Donec dolor arcu, ullamcorper at tincidunt at, auctor sit amet ipsum.</p>
+          </div>
+        </div>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Aboutkrunk"
+};
+</script>
+
+<style lang="scss">
+.about-krunk {
+  .about-hdr {
+    h1 {
+      font-size: 5rem;
+      color: #b23c52;
+      margin-bottom: 0;
+      margin-top: 3rem;
+
+      @media (min-width: 768px) {
+        margin-top: 1rem;
+      }
+
+      @media (min-width: 1024px) {
+        font-size: 6.7rem;
+        margin-top: 0;
+      }
+    }
+  }
+
+  .about-main {
+    background: #fff;
+    padding: 7rem 0;
+    margin-top: 6rem;
+    -webkit-box-shadow: 0px 3px 5px 1px rgba(0, 0, 0, 0.25);
+    -moz-box-shadow: 0px 3px 5px 1px rgba(0, 0, 0, 0.25);
+    box-shadow: 0px 3px 5px 1px rgba(0, 0, 0, 0.25);
+
+    .content-wrapper {
+      width: 90%;
+      margin: 0 auto;
+
+      @media screen and (min-width: 768px) {
+        width: 70%;
+      }
+
+      h2 {
+        font-family: "dnk";
+        color: #b23c52;
+
+        @media (min-width: 768px) {
+          font-size: 42px;
+        }
+      }
+
+      p {
+        margin-top: 4rem;
+        margin-bottom: 0;
+        font-size: 16px;
+        font-family: "Open Sans", sans-serif;
+        font-size: 16px;
+        color: #290540;
+      }
+    }
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <AboutKrunk />
   </div>
 </template>
+
+<script>
+import AboutKrunk from "@/components/AboutKrunk";
+
+export default {
+  name: "About",
+  components: {
+    AboutKrunk
+  }
+};
+</script>
+
+<style lang="scss">
+.about {
+  background: #f9b450;
+  background-image: url("../assets/about/about-buildings.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: bottom;
+  padding: 6rem 0 10rem 0;
+
+  @media screen and (min-width: 1200px) {
+    padding-bottom: 15rem;
+  }
+}
+</style>
